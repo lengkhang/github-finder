fc3e15066583f8a94c7ab93b01a4df698585d866
lengkhang <lengkhang@gmail.com>
2021-04-10 17:43:16 +0800

    Clean up

--- frontend/src/actions/saga.js
+++ frontend/src/actions/saga.js
@@ -6,7 +6,6 @@ function* fetchData(payload) {
     const { data } = payload;
     const { texts, type, pageNo = 1, pageSize } = data;
 
-    //TODO: EncodeURIComponent
     const query = `${type}=${encodeURIComponent(texts)}`;
 
     const response = yield fetch(`${process.env.REACT_APP_API_URL}/search?${query}&pageSize=${pageSize}&pageNo=${pageNo}`);
