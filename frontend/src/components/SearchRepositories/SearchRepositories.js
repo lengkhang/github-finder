6201bdf301f20c0ab7b57d11e278d3d74b36d4e1
lengkhang <lengkhang@gmail.com>
2021-04-11 15:12:15 +0800

    Fix unclear search repository when change navigation

--- frontend/src/components/SearchRepositories/SearchRepositories.js
+++ frontend/src/components/SearchRepositories/SearchRepositories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Input, Select, Typography } from 'antd';
 import RepositoriesTable from '../RepositoriesTable/RepositoriesTable';
@@ -54,6 +54,12 @@ const SearchRepositories = () => {
 
     await onSearch({ pageNo: page });
   };
+
+  useEffect(async () => {
+    if (!currentPage) {
+      await dispatch(clearSearch());
+    }
+  }, []);
 
   return (
     <>
